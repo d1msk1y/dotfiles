d7a4365c463477f64edb5e2c4017c47f39a55d9e
d1msk1y <yatcenko.dmitriy@gmail.com>
2023-04-17 13:32:20 +0000

    Setting monitor layout up on system startup

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x606 --rotate normal --output HDMI-1 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate left
