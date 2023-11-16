92ee555c7adebaf038a3ad00a96d9206971619b2
d1msk1y <exect81@gmail.com>
2023-11-16 20:14:54 +0100

    Add theme switching scripts

---
+++ .theme-scripts/gruvbox-light.sh
@@ -0,0 +1,3 @@
+echo "gruvbox-light" > '/home/dmytro/polybar-collection/theme.sh'
+cat "/home/dmytro/.config/kitty/themes/Gruvbox Material Light Medium.conf" > '/home/dmytro/.config/kitty/current-theme.conf'
+export GTK_THEME="Gruvbox-Light-BL"
---
+++ .theme-scripts/gruvbox.sh
@@ -0,0 +1,3 @@
+echo "gruvbox" > '/home/dmytro/polybar-collection/theme.sh'
+cat '/home/dmytro/.config/kitty/themes/Gruvbox Dark Soft.conf' > '/home/dmytro/.config/kitty/current-theme.conf'
+export GTK_THEME="Grubvox-Dark-BL"
